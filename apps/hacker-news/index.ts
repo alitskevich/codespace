f5bdaeb5bed67039cc21b576a75be0f96b3a825a
Alex <alitskevich@gmail.com>
2024-03-14 18:30:51 +0200

    add hn app

---
+++ apps/hacker-news/index.ts
@@ -0,0 +1,27 @@
+import launchWeb from "arrmatura-web";
+import templates from "./index.xml";
+import ui from "arrmatura-ui";
+import defaults from "arrmatura-ui/resources";
+import assets from "./assets";
+import { urlParse } from "ultimus";
+
+const { params } = urlParse(window.location.href);
+
+export const baseURL = 'https://hacker-news.firebaseio.com/v0'
+
+export const feedsInfo = {
+  news: { title: 'News', pages: 10 },
+  newest: { title: 'Newest', pages: 12 },
+  ask: { title: 'Ask', pages: 2 },
+  show: { title: 'Show', pages: 2 },
+  jobs: { title: 'Jobs', pages: 1 }
+}
+
+export const validFeeds = Object.keys(feedsInfo)
+
+const config = {
+  resources: { ...defaults, assets, params },
+  components: [...ui, templates]
+};
+
+launchWeb(config);
---
+++ apps/hacker-news/tailwind.config.js
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./**/*.{xml,html}", "./node_modules/arrmatura-ui/**/*.{xml,html}"],
+};
